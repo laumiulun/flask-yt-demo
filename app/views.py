ca7f58202218342340cce9b65d8d39978db199ab
Andy Lau <laumiulunn@gmail.com>
2022-02-14 20:01:14 -0700

    updated to include Js and CSS template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
 from app import app
 
-from flask import render_template, request
+from flask import render_template, request, redirect
+
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+
+    if request.method == "POST":
+
+        if request.files:
+
+            image = request.files["image"]
+
+            print(image)
+
+            return redirect(request.url)
+
+    return render_template("public/upload_image.html")
 
 
 @app.route("/")
@@ -15,3 +31,14 @@ def index():
         return render_template("public/index.html", args=args)
 
     return render_template("public/index.html", args=args)
+
+
+
+@app.route("/jinja")
+def jinja():
+    return render_template("public/jinja.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("public/about.html")
